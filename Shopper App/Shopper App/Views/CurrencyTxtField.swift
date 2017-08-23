f6fe9b5dcfcd953381e290ba6691b5e888a1e490
Damian <damian@Damians-MacBook-Pro.local>
2017-08-23 20:02:19 +0200

    Lesson01 commit

---
+++ Shopper App/Shopper App/Views/CurrencyTxtField.swift
@@ -0,0 +1,30 @@
+//
+//  CurrencyTxtField.swift
+//  Shopper App
+//
+//  Created by Damian on 23.08.2017.
+//  Copyright © 2017 Damian. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+
+        if let p = placeholder {
+            let place = NSAttributedString(string: p,
+                                           attributes: [.foregroundColor: UIColor.white])
+            attributedPlaceholder = place
+            textColor = UIColor.white
+        }
+        
+        
+    }
+
+}
